20e9caae5d4a432d7000a04f74670ce8979911f0
joshua-odero <misikojodero@gmail.com>
2026-03-02 20:40:09 +0300

    First commit: book.py and coffee.py

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,32 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    #Initialize object instance attributes(title and page count)
+    def __init__(self,title,page_count):
+        self.title = title
+        self.page_count = page_count
     
+    #Create get_page_count and set_page_count methods to controll page count property
+    def get_page_count(self):
+        return self._page_count
+    
+    def set_page_count(self,count):
+        if not isinstance(count, int):
+            print(f"page_count must be an integer")
+        else:
+            self._page_count = count
+    #Create turn_page() method and print something
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
+    #page count property now controlled by get_size and set_size methods with Python's property()
+    page_count = property(get_page_count, set_page_count)
+
+#Test cases
+""" title = input("Enter the book title: ")
+page_count = input("Enter the book's page count: ")
+
+book = Book(title,page_count)
+
+print(book.turn_page()) """
         
--- lib/coffee.py
+++ lib/coffee.py
@@ -1,4 +1,34 @@
 #!/usr/bin/env python3
 
 class Coffee:
-    pass
+    #Initialize object instance attributes(size and price)
+    def __init__(self,size,price):
+        self.size = size
+        self.price = price
+
+    #Create get_size and set_size methods to controll size property
+    def get_size(self):
+        return self._size
+    
+    def set_size(self, size_input):
+        if size_input == "Small" or size_input == "Medium" or size_input=="Large":
+            self._size = size_input
+        else:
+            print("size must be Small, Medium, or Large")
+    
+    #Create tip() method to add 1 to coffee price
+    def tip(self):
+        print("This coffee is great, here’s a tip!")
+        self.price = self.price + 1
+        return self.price
+
+    #Size property controlled by get_size and set_size methods
+    size = property(get_size,set_size)
+
+#Test cases
+""" size = input("Enter the coffee size: ")
+price = input("Enter the coffee price: ")
+
+coffee = Coffee(size,int(price))
+
+print(coffee.tip()) """
